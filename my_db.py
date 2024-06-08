5692d2ff88b1c55d7a5b17b0d3b21d199cc83a49
hisert <100667448+hisert@users.noreply.github.com>
2024-06-08 10:25:43 +0300

    Update my_db.py

--- my_db.py
+++ my_db.py
@@ -3,10 +3,12 @@ import socket
 import threading
 import signal
 import sys
+import os
 
 class JSONDatabase:
     def __init__(self, filename="data.json"):
         self.filename = filename
+        self.lock = threading.Lock()  # Thread güvenliği için kilit
 
     def load_data(self):
         try:
@@ -21,13 +23,22 @@ class JSONDatabase:
             json.dump(data, file, indent=4)
 
     def set_data(self, key, value):
-        data = self.load_data()
-        data[key] = value
-        self.save_data(data)
+        with self.lock:
+            data = self.load_data()
+            data[key] = value
+            self.save_data(data)
 
     def get_data(self, key):
-        data = self.load_data()
-        return data.get(key, None)
+        with self.lock:
+            data = self.load_data()
+            return data.get(key, None)
+
+    def del_data(self, key):
+        with self.lock:
+            data = self.load_data()
+            if key in data:
+                del data[key]
+                self.save_data(data)
 
 class TCPServer:
     def __init__(self, port, message_handler):
@@ -54,12 +65,15 @@ class TCPServer:
 
     def open_server_socket(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Portu hızla yeniden kullanmak için
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # Ek satır
         self.server_socket.bind(("", self.port))  # Tüm arayüzlerde dinle
         self.server_socket.listen(5)
 
     def close_server_socket(self):
         if self.server_socket:
+            for client in self.clients:
+                client.close()
             self.server_socket.close()
 
     def handle_client(self, client_socket):
@@ -102,6 +116,8 @@ def message_handler(message):
             server.send_to_all(f"({getted_data})")
         else:
             server.send_to_all("(NULL)")
+    elif parsed_data[0] == "DEL":
+        db.del_data(parsed_data[1])
 
 db = JSONDatabase()
 server = TCPServer(4040, message_handler)
